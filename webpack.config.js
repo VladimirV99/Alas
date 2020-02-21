d177baa93823fb3b931f8df0cf487623efe2c096
Vladimir Vuksanovic <vladevuksan99@gmail.com>
2020-02-21 16:19:54 +0100

    Split up styles. Load styles before scripts

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,11 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   target: 'web',
-  mode: 'production',
   entry: {
     index: './src/index/index.js',
     courses: './src/courses/courses.js',
@@ -24,7 +24,7 @@ module.exports = {
       {
         test: /\.css$/,
         use: [
-          "style-loader",
+          MiniCssExtractPlugin.loader,
           "css-loader"
         ]
       }
@@ -36,49 +36,53 @@ module.exports = {
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      template: './src/index/index.html',
+      template: 'src/index/index.html',
       inject: true,
       chunks: ['index'],
       filename: 'index.html'
     }),
     new HtmlWebpackPlugin({
-      template: './src/courses/courses.html',
+      template: 'src/courses/courses.html',
       inject: true,
       chunks: ['courses'],
       filename: 'courses.html'
     }),
     new HtmlWebpackPlugin({
-      template: './src/about/about.html',
+      template: 'src/about/about.html',
       inject: true,
       chunks: ['about'],
       filename: 'about.html'
     }),
     new HtmlWebpackPlugin({
-      template: './src/uoar1/arithmetics/arithmetics.html',
+      template: 'src/uoar1/arithmetics/arithmetics.html',
       inject: true,
       chunks: ['arithmetics'],
       filename: 'uoar1/arithmetics.html'
     }),
     new HtmlWebpackPlugin({
-      template: './src/uoar1/converter/converter.html',
+      template: 'src/uoar1/converter/converter.html',
       inject: true,
       chunks: ['converter'],
       filename: 'uoar1/converter.html'
     }),
     new HtmlWebpackPlugin({
-      template: './src/uoar1/error_correction/error_correction.html',
+      template: 'src/uoar1/error_correction/error_correction.html',
       inject: true,
       chunks: ['error_correction'],
       filename: 'uoar1/error_correction.html'
     }),
     new HtmlWebpackPlugin({
-      template: './src/uoar1/ieee754/ieee754.html',
+      template: 'src/uoar1/ieee754/ieee754.html',
       inject: true,
       chunks: ['ieee754'],
       filename: 'uoar1/ieee754.html'
     }),
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].css',
+      chunkFilename: '[id].css',
+    }),
     new CopyWebpackPlugin([
-      { from: './src/images', to: 'images' }
+      { from: 'src/images', to: 'images' }
     ])
   ]
 };
