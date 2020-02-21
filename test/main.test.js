d747a0eae4fc0570c39f352e75ae6e4477dc7374
Vladimir Vuksanovic <vladevuksan99@gmail.com>
2020-02-21 21:17:45 +0100

    Added initial test runner code

---
+++ test/main.test.js
@@ -0,0 +1,1 @@
+require('./uoar_core.test');
---
+++ test/uoar_core.test.js
@@ -0,0 +1,9 @@
+let expect = require('chai').expect;
+
+describe('UOAR Core', function() {
+
+    it('works', function() {
+        expect(true).to.be.true;
+    });
+
+});
